fdc9ea0bfa52563a5a8fc41d124b5051a8dd08a5
Matheus Alves <matheusalves.aa@outlook.com>
2020-01-15 12:06:51 -0300

    primeiro commit

---
+++ intro.py
@@ -0,0 +1,11 @@
+from classe import Caneta
+
+
+c1 = Caneta('Bic Cristal', 'Azul', 0.5, 80, True)
+c1.rabiscar('alguma coisa')
+c1.rabiscar('alguma coisa')
+c1.parar_rabiscar()
+c1.parar_rabiscar()
+c1.rabiscar('alguma coisa')
+
+print(f'A cor da caneta é {c1.cor}')
